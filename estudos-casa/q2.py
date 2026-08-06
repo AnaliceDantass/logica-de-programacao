63fe9dbd7eefd4c7f52ae1ab87d5fa3b5ab54d33
Analice <analicedantas07@gmail.com>
2026-08-06 10:25:41 -0300

    Delete estudos-casa/q2.py

--- estudos-casa/q2.py
+++
@@ -1,7 +0,0 @@
-nome = input("digite seu nome: ")
-idade = int(input("digite sua idade:"))
-ano = str((2026 - idade) + 65)
-
-if idade == 65:
-    print(f"O trabalhador {nome} poderá se aposentar no ano de {ano}")
-
